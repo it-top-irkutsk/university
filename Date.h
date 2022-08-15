c4d6d439ad3693898b5ad70237200fd492f3df50
Starinin Andrey <starininandrey@gmail.com>
2022-08-15 10:55:32 +0300

    Add ToString()

--- Date.h
+++ Date.h
@@ -1,7 +1,19 @@
 #pragma once
 
+#include <string>
+
+using namespace std;
+
 struct Date {
     int year;
     int month;
     int day;
+
+    string ToString() {
+        return to_string(day) + "." + to_string(month) + "." + to_string(year);
+    }
 };
+
+string DateToString(Date date) {
+    return to_string(date.day) + "." + to_string(date.month) + "." + to_string(date.year);
+}
